704a6b30219cec16d74c5455c4d3a89f7a351a70
Matt Eva <matteweva@gmail.com>
2021-12-13 08:34:47 -0800

    commit tree 43bc82425c3a61ac93dff87fc9be731a888ff736

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
+    if params.has_key?("name")
+      students = Student.where(first_name: params[:name].capitalize).or(Student.where(last_name: params[:name].capitalize))
+      render json: students
+    else
     students = Student.all
     render json: students
+    end
+    # byebug
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
